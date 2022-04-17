3be687bf5de2351637e9f1b4a514db447f4e4b51
Lord-sarcastic <adeoti.15.jude@gmail.com>
2022-04-17 07:14:38 +0100

    Feature: Added support for extra args in `make_request`

--- pokemon/services.py
+++ pokemon/services.py
@@ -3,10 +3,10 @@ from typing import Any, Dict
 import requests
 
 
-def make_request(url) -> Dict[Any, Any]:
+def make_request(url, **kwargs) -> Dict[Any, Any]:
     """
     Non-generic utility to make a request to the given URL.
     """
-    response = requests.get(url)
+    response = requests.get(url, **kwargs)
     response.raise_for_status()
     return response.json()
